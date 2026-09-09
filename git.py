20efc550be8e9ad08a9a9b6153fa33611a9c8e7f
mittariq <61346403+mittariq@users.noreply.github.com>
2026-09-09 16:59:51 -0400

    ADDED TENSORS OPERATIONS
    Added the addition, subtraction and multiplication operations

--- git.py
+++ git.py
@@ -5,3 +5,14 @@ x = torch.tensor(x)
 y = ast.literal_eval(input("enter another tensor"))
 y = torch.tensor(y)
 print(f"your tesnors are {x} and {y}")
+o= input("enter operation add, subtract, multiply")
+try : 
+  if "add" in o:
+    print(f"tensor addition is {x + y}")
+  if "sub" in o:
+    print(f"tensor subtraction is {x - y}")
+  if "mul" in o:
+    print(f"tensor multipltion is {x @ y}")
+except:
+  print("error")
+  
